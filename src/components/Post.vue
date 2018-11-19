a1e903045d1fbc116aaf5b33dda4a61c7566edb0
Javier Llerenas <javr.llerenas@gmail.com>
2018-11-19 12:02:00 -0800

    feat: create simple Post component footprint

---
+++ src/components/Post.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="section">
+        <article class="media">
+
+            <figure class="media-left">
+                <img src="@/assets/images/submissions/image-yellow.png" alt="submission image" class="image is-64x64">
+            </figure>
+
+            <div class="media-content">
+                <div class="content">
+                    <p>
+                        <strong>
+                            <a href="#" class="has-text-info">Yellow Pal</a>
+                            <span class="tag is-small">#4</span>
+                        </strong>
+                        <br>
+                            On-demand sand castle construction expertise.
+                        <br>
+                        <small class="is-size-7">
+                            Submitted by: 
+                            <img class="image is-24x24"
+                                src="@/assets/images/avatars/daniel.jpg">
+                        </small>
+                    </p>
+                </div>
+            </div>
+
+            <div class="media-right">
+                <span class="icon is-small">
+                    <i class="fa fa-chevron-up"></i>
+                    <strong class="has-text-info">10</strong>
+                </span>
+            </div>
+
+        </article>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+// STYLE
+<style>
+
+.media {
+    max-width: 600px;
+    margin: 0 auto;
+    border: 1px solid #e6e7e9;
+    padding: 1em 1.5em 0.5em 1.5em;
+    border-radius: 0.3em;
+}
+</style>
---
+++ src/components/UpVote.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div>
+            <h2 class="title has-text-centered dividing-header">UpVote! 💯</h2>
+        </div>
+
+        <Post></Post>
+        
+        <footer>
+            <img src="https://bulma.io/images/made-with-bulma.png" alt="Made with Bulma" width="128" height="24">
+        </footer>
+    </div>
+</template>
+
+<script>
+import Post from '@/components/Post'
+
+export default {
+    components: {
+        Post
+    }    
+}
+</script>
